db7c99f7fbd65b5e79384ece2a714a9024bcb364
Almas Baimagambetov <almaslvl@gmail.com>
2021-01-11 13:09:45 +0000

    added full tut

--- src/main/java/com/almasb/fosdem/BasicGameApp.java
+++ src/main/java/com/almasb/fosdem/BasicGameApp.java
@@ -1,11 +1,17 @@
 package com.almasb.fosdem;
 
+import com.almasb.fxgl.animation.Interpolators;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.SpawnData;
+import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
+
+import java.util.Map;
 
 import static com.almasb.fosdem.EntityType.*;
 import static com.almasb.fxgl.dsl.FXGL.*;
@@ -28,25 +34,65 @@ public class BasicGameApp extends GameApplication {
     }
 
     @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("score", 0);
+    }
+
+    @Override
     protected void initGame() {
         getGameWorld().addEntityFactory(new BasicFactory());
 
         player = spawn("player", 0, 450);
 
         run(() -> {
-            spawn("goodStuff", random(0, 500), 0);
-            spawn("badStuff", random(0, 500), 0);
+            spawnFadeIn(
+                    "goodStuff",
+                    new SpawnData(random(0, 500), 0),
+                    Duration.seconds(1)
+            );
+
+            spawnWithScale(
+                    "badStuff",
+                    new SpawnData(random(0, 500), 0),
+                    Duration.seconds(3),
+                    Interpolators.ELASTIC.EASE_OUT()
+            );
+
         }, Duration.seconds(0.5));
     }
 
     @Override
     protected void initPhysics() {
         onCollisionBegin(PLAYER, GOOD_STUFF, (p, stuff) -> {
+            inc("score", +1);
             stuff.removeFromWorld();
         });
 
         onCollisionBegin(PLAYER, BAD_STUFF, (p, stuff) -> {
+            inc("score", -2);
             stuff.removeFromWorld();
+        });
+    }
+
+    @Override
+    protected void initUI() {
+        var text = getUIFactoryService().newText("", Color.BLUE, 24.0);
+        text.setTranslateX(50);
+        text.setTranslateY(50);
+        text.textProperty().bind(getip("score").asString());
+
+        addUINode(text);
+
+        getip("score").addListener((o, old, newScore) -> {
+            animationBuilder()
+                    .interpolator(Interpolators.BOUNCE.EASE_OUT())
+                    .duration(Duration.seconds(0.4))
+                    .repeat(2)
+                    .autoReverse(true)
+                    .scale(text)
+                    .from(new Point2D(1, 1))
+                    .to(new Point2D(2, 2))
+                    .buildAndPlay();
         });
     }
 
